01843fb883a8bcd8e5a2b3f701e018e0e1bfcc1a
rs-ibm <nick.freer@uk.ibm.com>
2024-02-04 10:10:51 +0000

    added db_loader.py

--- database/db_loader.py
+++ database/db_loader.py
@@ -1,95 +1,101 @@
+#!/usr/bin/python3
 import psycopg2
-db_name='dvdrental'
-db_host='ndf-db-postgress1.fyre.ibm.com'
-db_user='postgres'
-db_pass='dbpassw0rd'
+import csv
+import re
+#==================================================================
+#                             FUNCTIONS
+#==================================================================
+
+# function to build start and stop order lists for each application
+def build_list(orderObj,order,id,application,environment):
+    theKey=f'{application}@{environment}'
+    try:
+        if orderObj[theKey]:
+            pass
+    except KeyError:
+        orderObj[theKey]=[]
+    if len(orderObj[theKey]) <= int(order):
+        orderObj[theKey].extend([''] * (int(order) - len(orderObj[theKey]) +1))
+    orderObj[theKey][int(order)]=str(id)
+
+# function to insert VM entries
+def insertvm(vms,cursor,dnsname,startcmd,stopcmd,statuscmd):
+    for i in range(len(vms)) :
+        if vms[i][1]== dnsname:
+            print('NAME')
+            if vms[i][2] != startcmd or vms[i][3] != stopcmd or vms[i][4] != statuscmd:
+                print('NO MATCH')
+                print(f"UPDATE vms SET startcmd = '{startcmd}', stopcmd = '{stopcmd}', statuscmd = '{statuscmd}' WHERE id = {vms[i][0]}")
+                cursor.execute(f"UPDATE vms SET startcmd = '{startcmd}', stopcmd = '{stopcmd}', statuscmd = '{statuscmd}' WHERE id = {vms[i][0]}")
+            return vms[i][0]
+    cursor.execute(f"INSERT INTO vms (dnsname, startcmd, stopcmd, statuscmd) VALUES ('{dnsname}','{startcmd}','{stopcmd}','{statuscmd}');" )
+    cursor.execute(f"SELECT id, dnsname FROM vms where (dnsname = '{dnsname}');" )
+    newvm=cursor.fetchone()
+    return newvm[0]
+
+# function to insert App entries
+def insertapp(apps,cursor,start,stop):
+    for theKey in start.keys():
+        match=False
+        print(theKey)
+        startorder=' '.join(start[theKey]).strip()
+        stoporder=' '.join(stop[theKey]).strip()
+        print (f'{startorder}  {stoporder}')
+        print(re.split(' +',startorder))
+        print(re.split(' +',stoporder))
+        for i in range(len(apps)):
+            if f'{apps[i][1]}@{apps[i][2]}'==theKey :
+                match=True
+                print('NAME')
+                if apps[i][3] != startorder or apps[i][4] != stoporder:
+                    print('NO MATCH')
+                    print(f"UPDATE apps SET startorder = '{startorder}', stoporder = '{stoporder}' WHERE id = {apps[i][0]}")
+                    cursor.execute(f"UPDATE apps SET startorder = '{startorder}', stoporder = '{stoporder}' WHERE id = {apps[i][0]}")
+                break
+        if not match:
+            uniqname=theKey.split('@')
+            print(f"INSERT INTO apps (appname, environment, startorder, stoporder) VALUES ('{uniqname[0]}','{uniqname[1]}','{startorder}','{stoporder}');" )
+            cursor.execute(f"INSERT INTO apps (appname, environment, startorder, stoporder) VALUES ('{uniqname[0]}','{uniqname[1]}','{startorder}','{stoporder}');" )
+
+#==================================================================
+#                               MAIN
+#==================================================================
+
+db_name='apps'
+db_host='ndf-dbtest1.fyre.ibm.com'
+db_user='admin'
+db_pass='adm1nPa55'
 db_port=5432
 conn = psycopg2.connect(database=db_name,
                         host=db_host,
                         user=db_user,
                         password=db_pass,
                         port=db_port)
-# As you can see, there are quite a few arguments you need to define in order to connect to the database. Here's a quick summary of what each of these arguments means:
-
-# database: the name of the database that you want to connect to (you can only connect to one database with one connection object)
-# host: this is probably an IP address or a URL that points to the database server (e.g., xyz.example.com)
-# user: the name of the PostgreSQL user
-# password: the matching password for that user
-# port: the port that the PostgreSQL server uses (usually 5432 if the server is hosted locally but can be other)
-# If you submitted the correct database credentials, you get a live database connection object that you can use to create a cursor object.
-
-# A cursor object will help you execute any queries on the database and retrieve data. Here's how to create a cursor object:
 
 cursor = conn.cursor()
-# Now let's query the database using the cursor we just created:
+cursor.execute("SELECT * FROM apps")
+apps=cursor.fetchall()
+cursor.execute("SELECT * FROM vms")
+vms=cursor.fetchall()
 
-# cursor.execute("SELECT count(*) FROM actor")
-cursor.execute("SELECT * FROM actor")
-# We use the execute() function and submit a query string as its argument. This query that we submitted will be run against the database. It's important to note that after the query executes, you still need to use one of the Psycopg2 functions to retrieve data rows:
+start={}
+stop={}
+with open('dbinput.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(f'row {row}')
+        id=insertvm(vms,cursor,row['dnsname'],row['start cmd'],row['stop cmd'],row['status cmd'])
+        print(f'ID: {id}')
+        build_list(start,row['start order'],id,row['application'],row['environment'])
+        build_list(stop,row['stop order'],id,row['application'],row['environment'])
+    print(f'start {start}')
+    print(f'stop {stop}')
 
-# fetchone()
-# fetchall()
-# fetchmany()
-# Let's see how each of them works!
+print(vms)
 
-# Example: fetchone()
-# The most basic way to fetch data from your database is to use the fetchone() function. This function will return exactly one row — the first row — after executing the SQL query.
+insertapp(apps,cursor,start,stop)
 
-# Here's an example:
-# row=cursor.fetchone()
-# print(row)
-# print(row[0])
-# print(row[1])
-# print(row[2])
-# print(row[3])
 
-# theRest=cursor.fetchall()
-# print(theRest)
-# print(theRest[0][1])
-# print(theRest[1][1])
-# print(theRest[2][2])
-# print(theRest[3][2])
 
-for actor in cursor.fetchall():
-    print(actor)
-    print (f'{actor[1]} {actor[2]}')
-
-# (1, 'Budapest', 'newly-built', 'after 2011', 30, 1)
-# In this example, fetchone() returns one row from the database in the form of a tuple where the order of your data in the tuple will be based on the order of the columns you specified in the query.
-
-# Because of this, it's important to make sure you specify the order of columns properly when you create the query string so you know which data is which in the tuple.
-
-# Example: fetchall()
-# What if you need more than just one row from your database? What if you need 10, 100, 1000, or more rows? You can use the fetchall() Psycopg2 function, which works the same way as fetchone() except that it returns not just one row as a result but all of them.
-
-# print(cursor.fetchall())
-# print(cursor.fetchone())
-
-# [(1, 'Budapest', 'newly-built', 'after 2011', 30, 1),
-#  (2, 'Budapest', 'newly-built', 'after 2011', 45, 2),
-#  (3, 'Budapest', 'newly-built', 'after 2011', 32, 2),
-#  (4, 'Budapest', 'newly-built', 'after 2011', 48, 2),
-#  (5, 'Budapest', 'newly-built', 'after 2011', 49, 2),
-#  (6, 'Budapest', 'newly-built', 'after 2011', 49, 2),
-#  (7, 'Budapest', 'newly-built', 'after 2011', 71, 3),
-#  (8, 'Budapest', 'newly-built', 'after 2011', 50, 2),
-#  (9, 'Budapest', 'newly-built', 'after 2011', 50, 2),
-#  (10, 'Budapest', 'newly-built', 'after 2011', 57, 3)]
-# [...]
-# Notice how we get more rows back, not just one.
-
-# Example: fetchmany()
-# With fetchmany(), you have another option to retrieve multiple records from the database and have more control over the exact amount of rows retrieved.
-
-# print(cursor.fetchmany(size=5))
-# [(1, 'Budapest', 'newly-built', 'after 2011', 30, 1),
-#  (2, 'Budapest', 'newly-built', 'after 2011', 45, 2),
-#  (3, 'Budapest', 'newly-built', 'after 2011', 32, 2),
-#  (4, 'Budapest', 'newly-built', 'after 2011', 48, 2),
-#  (5, 'Budapest', 'newly-built', 'after 2011', 49, 2)]
-# Here we only receive five rows because we set the size argument to 5. This function gives you more control on a code-level over how many rows to return from your database table.
-
-# Wrapping Up
-# After you're finished querying your database and using the connection object in your Python code, make sure to always close the connection using conn.close().
-
+conn.commit() 
 conn.close()
