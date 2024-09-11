327e815e42bbc7b8e4e70b4a119fc911b895e244
root <root@ndf-build1.fyre.ibm.com>
2024-09-11 12:16:07 -0700

    imagename

--- docker_image/build.sh
+++ docker_image/build.sh
@@ -1,1 +1,3 @@
-docker build -t nickfreer/exp-orchestration:0.0.3 .
+#docker build -t nickfreer/exp-orchestration:0.0.3 .
+docker build -t registry.gitlab.com/tivolinick1/hsbc/orchestration:0.0.3 .
+
--- docker_image/push.sh
+++ docker_image/push.sh
@@ -1,1 +1,5 @@
+#docker push nickfreer/orchestration:0.0.3
+docker push registry.gitlab.com/tivolinick1/hsbc/orchestration:0.0.3
+docker tag registry.gitlab.com/tivolinick1/hsbc/orchestration:0.0.3 nickfreer/orchestration:0.0.3
 docker push nickfreer/orchestration:0.0.3
+
